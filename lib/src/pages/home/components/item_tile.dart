6e3988c5ed0df5fda0f8216da1b1076c5c0cd8db
santana-tec <elder.santana.tec@gmail.com>
2022-07-28 16:04:29 -0300

    tela home concluida

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,32 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:lojinha/src/config/custom_colors.dart';
 import 'package:lojinha/src/models/item_model.dart';
+import 'package:lojinha/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
   final ItemModel item;
 
-  const ItemTile({
+   ItemTile({
     Key? key,
     required this.item,
   }) : super(key: key);
 
+  final UtilsServices utilsServices = UtilsServices();
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        children: [
-          // Image
-          Image.asset(item.imgUrl),
+    return Stack(
+      children: [
+        Card(
+          elevation: 2,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Image
+                Expanded(child: Image.asset(item.imgUrl)),
 
-          // Title
+                // Title
+                Text(
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
 
-          // Price
-        ],
-      ),
+                // Price
+
+                Row(
+                  children: [
+                    Text(
+                      utilsServices.priceToCurency(item.price),
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.customSawtchColor,
+                      ),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: (){
+
+            },
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                color: CustomColors.customSawtchColor,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(15),
+                ),
+              ),
+              child: const Icon(
+                Icons.shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
